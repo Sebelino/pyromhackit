f7e7219fd2d76f9764f75dc93d85d93a5cd14f02
Sebelino <sebelino7@gmail.com>
2015-05-30 20:07:02 +0200

    Made the textdumper more general, adding support for using different formats for input/output.

--- textdumper.py
+++ textdumper.py
@@ -14,34 +14,50 @@ def filecontents(path):
 		
 def read_yaml(path):
     """ YAML -> Dictionary. """
-    lines = filecontents(path)
     stream = open(path, 'r', encoding='utf8')
     dct = yaml.load(stream)
     return dct
 
-def pretty_table(matrix, encoder=dict()):
-    """ Readable representation of a list of lists, translating entries according to encoder. """
-    if len(matrix) == 0:
-        return u""
-    table = PrettyTable(header=False, border=True, padding_width=0)
-    for row in matrix:
-        filteredrow = row[::2]
-        encodedrow = tuple(encoder[x] if x in encoder else x for x in filteredrow)
-        table.add_row(encodedrow)
-    return str(table)
+def readbin(path):
+    bytes = []
+    with open(path, 'rb') as f:
+        bytestring = f.read(1)
+        while bytestring != b"":
+            byte = bytestring[0]
+            bytes.append(byte)
+            bytestring = f.read(1)
+    return bytes
 
+def tabulate(stream, cols):
+    table = PrettyTable(header=False, border=True, padding=0)
+    for i in range(0, len(stream), cols):
+        row = stream[i:i+cols]
+        table.add_row(row)
+    return table
+
+def convert(infile, informat, outformat, transliterationfile):
+    contents = readbin(infile)
+    charmap = read_yaml(transliterationfile) if transliterationfile else dict()
+    contents = [ord(charmap[b]) if b in charmap else b for b in contents]
+    if outformat == "hex":
+        contents = [hex(n)[2:].upper() for n in contents]
+    elif outformat == "text":
+        contents = [chr(n) for n in contents]
+    output = tabulate(contents, 16)
+    return output
+    
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="Translate hex -> letters.")
-    parser.add_argument('file', help="Path to the file containing the hex.")
-    parser.add_argument('--output', '-o', help="Output to file instead of to console.")
+    parser = argparse.ArgumentParser(description="Translate one format to another format.")
+    parser.add_argument('infile', help="Path to the file containing the hex.")
+    parser.add_argument('informat', choices=["bin", "hex"], help="Format of the read file.")
+    parser.add_argument('outformat', choices=["hex", "text"], help="Format of the output.")
+    parser.add_argument('--transliteration', '-t', help="YAML file mapping the read bytes into characters.")
+    parser.add_argument('--outfile', '-o', help="Output to file instead of to console.")
+
     args = parser.parse_args()
 
-    lines = filecontents(args.file)
-    hex = [line.split() for line in lines]
-    hex = [tuple(int(s, 16) for s in row) for row in hex]
-    hexmap = read_yaml('hexmap.yaml')
-    table = pretty_table(hex, hexmap)
+    out = convert(args.infile, args.informat, args.outformat, args.transliteration)
     f = sys.stdout
-    if args.output:
-        f = open(args.output, 'w', encoding="utf8")
-    print(table, file=f)
+    if args.outfile:
+        f = open(args.outfile, 'w', encoding="utf8")
+    print(out, file=f)

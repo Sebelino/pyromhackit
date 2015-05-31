e3e5e1a1195fb9717d8dd7997d2650374d461110
Sebelino <sebelino7@gmail.com>
2015-05-31 15:46:51 +0200

    Refactored reader.py and orthogonalized the interface.

--- reader.py
+++ reader.py
@@ -29,12 +29,13 @@ def tabulate(stream, cols):
     """ Display the stream of characters in a table. """
     table = PrettyTable(header=False, border=True, padding=0)
     for i in range(0, len(stream), cols):
-        row = stream[i:i+cols]
+        segment = stream[i:i+cols]
+        row = segment+[" "]*max(0, cols-len(segment))
         table.add_row(row)
     return table
 
 def compact(stream, cols):
-    """ Output every character with nothing inbetween, making a line break every cols characters.
+    """ Output every character with nothing in-between, making a line break every cols characters.
         If cols = 0, output everything on one line. """
     output = []
     for i in range(len(stream)):
@@ -43,45 +44,54 @@ def compact(stream, cols):
         output.append(stream[i])
     return "".join(output).strip()
     
-def minimal(stream, cols):
-    """ Same as compact(...), but also removes all whitespace. """
-    output = compact(stream, cols)
-    for s in string.whitespace.replace("\n", "").replace("\r", ""):
-        output = output.replace(s, "")
+def pack(stream):
+    """ Removes all whitespace. """
+    whitespace = string.whitespace.replace("\n", "").replace("\r", "")
+    output = [e for e in stream if e not in whitespace]
     return output
-    
-def convert(infile, inencoding, outencoding, outformat, columns, transliterationfile):
-    """ Uses the inencoding to read the infile, does some transliteration and outputs it in outencoding using outformat. """
+
+def convert(infile, inencoding, pipeline, width, transliterationfile):
+    """ Uses the inencoding to read the infile, does some transliteration and applies a sequence of additional processing filters. """
     contents = readbin(infile)
     charmap = read_yaml(transliterationfile) if transliterationfile else dict()
     contents = [ord(charmap[b]) if b in charmap else b for b in contents]
-    if outencoding == "hex":
-        contents = [hex(n)[2:].upper() for n in contents]
-    elif outencoding == "text":
-        contents = [chr(n) for n in contents]
-    if outformat == "table":
-        output = tabulate(contents, columns)
-    elif outformat == "compact":
-        output = compact(contents, columns)
-    elif outformat == "minimal":
-        output = minimal(contents, columns)
-    return output
+    contenttype = [int]
+    pipe = {
+        "hex": ([int], [str], lambda lst: [("0"+hex(n)[2:])[-2:].upper() for n in lst]),
+        "text": ([int], [str], lambda lst: [chr(n) for n in lst]),
+        "odd": (None, None, lambda lst: lst[::2]),
+        "table": (None, str, lambda lst: tabulate(lst, width)),
+        "pack": ([str], str, pack),
+    }
+    for filter in pipeline:
+        (inputtype, outputtype, f) = pipe[filter]
+        if inputtype not in [None,contenttype]:
+            raise Exception("Filter %s expected type %s, got %s" % (filter, inputtype, contenttype))
+        contents = f(contents)
+        if outputtype is not None: contenttype = outputtype
+    if isinstance(contents, list):  # If still a list, make it into a string
+        contents = compact(contents, width)
+    return contents
     
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Translate one format to another format.")
     parser.add_argument('infile', help="Path to the file containing the hex.")
-    parser.add_argument('inencoding', choices=["bin", "hex"], help="Encoding of the read file.")
     """ Encoding: The appearance of the characters themselves (hexadecimal, ASCII, etc.) """
-    parser.add_argument('outencoding', choices=["hex", "text"], help="Encoding of the output.")
-    """ Format: The way the individual characters are output (in a table, on one line, etc.) """
-    parser.add_argument('outformat', choices=["table", "compact", "minimal"], help="Format of the output.")
-    parser.add_argument('columns', type=int, help="Number of columns in the output.")
+    parser.add_argument('inencoding', choices=["bin", "hex"], help="Encoding of the read file.")
     parser.add_argument('--transliteration', '-t', help="YAML file mapping the read bytes into characters.")
+    parser.add_argument('process', nargs='*',
+                        choices=["odd",  # [X] -> [X] : Strip away the 2nd, 4th, 6th... character
+                                 "hex",  # [Int] -> [String] : Show the values in hexadecimal
+                                 "text",  # [Int] -> [String] : Convert the values to UTF-8 characters
+                                 "pack",  # [String] -> [String] : Strip away whitespace
+                                 "table",  # [String] -> String : Display the results in a table
+                                ],
+                        help="Apply processing rules to the out-encoded stream.")
+    parser.add_argument('--width', '-w', type=int, default=0, help="Number of columns in the output.")
     parser.add_argument('--outfile', '-o', help="Output to file instead of to console.")
 
     args = parser.parse_args()
-
-    out = convert(args.infile, args.inencoding, args.outencoding, args.outformat, args.columns, args.transliteration)
+    out = convert(args.infile, args.inencoding, args.process, args.width, args.transliteration)
     f = sys.stdout
     if args.outfile:
         f = open(args.outfile, 'w', encoding="utf8")

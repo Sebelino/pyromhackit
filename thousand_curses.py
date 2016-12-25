fb02fc1eef4143d9fe99d197e9d5ce46c260e94e
Sebastian Olsson <sebelino7@gmail.com>
2016-12-25 01:47:49 +0100

    Changed src window bgcolor.

--- thousand_curses.py
+++ thousand_curses.py
@@ -16,7 +16,7 @@ class Editor:
         self.stdscr = curses.initscr()
         curses.start_color()
         curses.use_default_colors()
-        curses.init_pair(1, curses.COLOR_CYAN, curses.COLOR_MAGENTA)
+        curses.init_pair(1, curses.COLOR_CYAN, curses.COLOR_BLACK)
         curses.init_pair(2, curses.COLOR_BLUE, curses.COLOR_YELLOW)
         curses.init_pair(3, curses.COLOR_BLACK, curses.COLOR_BLUE)
 

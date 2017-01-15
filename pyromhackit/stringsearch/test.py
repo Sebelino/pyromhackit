60c5723ea3c9277b2f736fa1ebdbe0963228eaf7
Sebastian Olsson <sebelino7@gmail.com>
2017-01-15 02:33:09 +0100

    Fixed stringsearch build error.

--- pyromhackit/stringsearch/test.py
+++ pyromhackit/stringsearch/test.py
@@ -3,6 +3,10 @@
 from nose.tools import assert_equals
 
 
+def locations(content):
+    return content.index(b"HELLO")
+
+
 def test_findscript():
     paramlist = [
         (b"\x00\x10\x42 HELLO \x89\x32", [(4, 5)]),

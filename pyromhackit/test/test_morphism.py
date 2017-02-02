96e40f70d8c59d5642cd2f8ca06b3a3f2b4c9d3b
Sebastian Olsson <sebelino7@gmail.com>
2017-02-02 23:39:53 +0100

    Added tests for impose methods and source_diffusion.

--- pyromhackit/test/test_morphism.py
+++ pyromhackit/test/test_morphism.py
@@ -4,6 +4,8 @@
 
 import os
 
+import pytest
+
 from ..rom import Morphism
 from ..thousandcurses.codec import ASCII
 
@@ -11,7 +13,7 @@ package_dir = os.path.dirname(os.path.abspath(__file__))
 
 
 class TestASCIIMorphism:
-    def setup_class(self):
+    def setup(self):
         self.morphism = Morphism(b"abcdefghi", ASCII)
 
     def test_repr(self):
@@ -22,3 +24,32 @@ class TestASCIIMorphism:
         expected = ("Morphism(b'abcdefghi',\n"
                     "          'abcdefghi')")
         assert str(self.morphism) == expected
+
+    @pytest.mark.parametrize("srcindex, expected", [
+        (0, {0}),
+        (1, {1}),
+        (8, {8}),
+    ])
+    def test_source_diffusion(self, srcindex, expected):
+        assert self.morphism.source_diffusion(srcindex) == expected
+
+    @pytest.mark.skip("TODO")
+    @pytest.mark.parametrize("byteidx, stridx, c, expected", [
+        (0, 0, 'K', ord('K')),
+        (0, 1, 'K', None),
+        (8, 8, 'a', ord('a')),
+        (0, 0, 'ä', None),
+    ])
+    def test_impose_character(self, byteidx, stridx, c, expected):
+        if expected is None:
+            assert self.morphism.impose_character(byteidx, stridx, c) is None
+        else:
+            returned = self.morphism.impose_character(byteidx, stridx, c)
+            msg = "Could not find a value of r[{}] so that s[{}] == {}".format(byteidx, stridx, c)
+            assert isinstance(returned, int), msg
+            assert returned == expected
+
+    @pytest.mark.skip("TODO (?)")
+    def test_impose_decoding(self):
+        newdecoder = self.morphism.impose_decoding(97, 'b')
+        assert newdecoder

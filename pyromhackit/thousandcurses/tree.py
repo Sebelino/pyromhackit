a0d226bd307a016f1758629c03d73c9a98eb12a1
Sebastian Olsson <sebelino7@gmail.com>
2017-03-22 23:40:05 +0100

    Working on new Tree class.

--- pyromhackit/thousandcurses/tree.py
+++ pyromhackit/thousandcurses/tree.py
@@ -13,7 +13,7 @@ class Tree(treelib.Tree):
         self.from_nested_container(arg)
 
     def from_nested_container(self, arg):
-        if not self.is_treelike(arg):
+        if not self.is_treelike(arg) or not self.type(arg) in {str, bytes}:
             raise ValueError(
                 "A tree is constructed from a non-empty nested iterable of iterables, strings, or bytestrings."
                 " The presence of a string is mutually exclusive with the presence of a bytestring.")
@@ -55,3 +55,31 @@ class Tree(treelib.Tree):
                 for child in element:
                     q.put(child)
         return True
+
+    @staticmethod
+    def type(tipe, pseudotree):
+        """ Returns the single type that every non-iterable element in the treelike structure shares, or None if there
+        exist elements with different types. """
+        container_types = {tuple, list, Tree}
+        content_types = {bytes, str}
+        q = Queue()
+        q.put(pseudotree)
+        content_type = None
+        while not q.empty():
+            element = q.get()
+            for tpe in content_types:
+                if isinstance(element, tpe):
+                    if not content_type or isinstance(element, content_type):
+                        content_type = tpe
+                        break
+                    elif not isinstance(element, content_type):
+                        return False
+            else:
+                if not any(isinstance(element, tpe) for tpe in container_types):
+                    return False
+                if len(element) == 0:
+                    return False
+                for child in element:
+                    q.put(child)
+        return True
+

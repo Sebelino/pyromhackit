c2f503ce3ff9b81bed7a54432aae985bec0c418e
Sebastian Olsson <sebelino7@gmail.com>
2017-05-28 23:46:37 +0200

    Implemented Topology.maketree prototype.

--- pyromhackit/tree.py
+++ pyromhackit/tree.py
@@ -8,9 +8,10 @@ package_dir = os.path.dirname(os.path.abspath(__file__))
 class Topology(object):
     def __init__(self, stringrepr: str):
         """ @stringrepr is the string representation of a certain Tree structure. @stringrepr is a '-' separated string
-        where each token is either a positive integer or the character '*'. If the ith token is an integer n, the ith
-        level of every Tree that belong to this Topology will consist of n nodes. If the token is '*', the level will
-        consist of any number of nodes. You can use this class to create a Tree from a (byte-)string. """
+        where each token is either a positive integer or the character '*'. If the ith token is an integer n, each
+        subtree in the (i-1)th level of any Tree belonging to this Topology will contain n children. If the token is
+        '*', the ith level will consist of any number of nodes. You can use this class to create a Tree from a
+        (byte-)string. """
 
         self.sizes = [None if t == '*' else int(t) for t in stringrepr.split("-")]
         assert self.sizes[0] is None  # Assuming the form *...-n for now
@@ -18,7 +19,12 @@ class Topology(object):
             assert isinstance(s, int)
 
     def maketree(self, iterable):
-        pass
+        if len(self.sizes) == 1:
+            return iterable
+        elif len(self.sizes) == 2:
+            lst = list(iterable)
+            s = self.sizes[1]
+            return list(zip(list(lst[i::s]) for i in range(s)))
 
     def indexpath(self, i):
         """ Returns the index path to the ith child for any Tree belonging to this Topology. """

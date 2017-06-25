53577ed28a06d25dc1acd09b1e7803ca75284bc6
Sebastian Olsson <sebelino7@gmail.com>
2017-06-25 23:35:51 +0200

    Adjusted Topology tests to new traverse_preorder.

--- pyromhackit/test/test_topology.py
+++ pyromhackit/test/test_topology.py
@@ -8,7 +8,7 @@ from pyromhackit.tree import SimpleTopology
 package_dir = os.path.dirname(os.path.abspath(__file__))
 
 
-class TestSimpleTwoToplogy(object):
+class TestSimpleTwoTopology(object):
     def setup(self):
         self.topology = SimpleTopology(2)
 
@@ -25,8 +25,8 @@ class TestSimpleTwoToplogy(object):
 
     @pytest.mark.parametrize("arg, expected", [
         (b'', []),
-        (b'ab', [((0,), b'ab')]),
-        (b'abcd', [((0,), b'ab'), ((1,), b'cd')]),
+        (b'ab', [(0, 0, (0,), b'ab')]),
+        (b'abcd', [(0, 0, (0,), b'ab'), (2, 1, (1,), b'cd')]),
     ])
     def test_traverse_preorder(self, arg, expected):
         assert list(self.topology.traverse_preorder(arg)) == expected

4279245046b228ed82c0755e8adf8e9fa7b41f51
Sebastian Olsson <sebelino7@gmail.com>
2017-07-10 23:46:47 +0200

    Implemented _compute_length in IndexedGMmap.

--- pyromhackit/rom.py
+++ pyromhackit/rom.py
@@ -169,6 +169,30 @@ class IndexedGMmap(GMmap):
         """ :return The slice for the bytestring that encodes the element(s) at location @location of the sequence. """
         raise NotImplementedError()
 
+    def _compute_length(self, content: mmap.mmap):
+        """ :return The length of the sequence which is computed in O(n*log(n)) by finding the lowest non-negative index
+        that raises an IndexError. """
+        try:
+            self._logicalint2physical(0)
+        except IndexError:
+            return 0
+        # Find upper bound:
+        for b in (2 ** i for i in itertools.count(1)):
+            try:
+                self._logicalint2physical(b)
+            except IndexError:
+                break
+        a = 0
+        while a < b:
+            # INV: 0 = a < avg + 1 = length < b
+            avg = a + (a + b) // 2
+            try:
+                self._logicalint2physical(avg)
+                a = avg
+            except IndexError:
+                b = avg - 1
+        return a + 1
+
 
 class Additive(metaclass=ABCMeta):
     @abstractmethod

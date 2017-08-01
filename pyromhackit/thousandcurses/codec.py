efe8f6bdd7c6a2950aa8e0d877f998285c07fc62
Sebastian Olsson <sebelino7@gmail.com>
2017-08-01 22:21:32 +0200

    Adjusted 1000curses codecs to the obsoletion of the Morphism paradigm.

--- pyromhackit/thousandcurses/codec.py
+++ pyromhackit/thousandcurses/codec.py
@@ -9,7 +9,6 @@ A decoding function maps a unicode string to a bytestring.
 Encoders/decoders are bijective -- each encoder has a inverse decoder function,
 and vice versa.
 """
-
 import sys
 import os
 import typing
@@ -21,6 +20,8 @@ import yaml
 from abc import ABC, abstractmethod
 import inspect
 
+from bidict import bidict
+
 package_dir = os.path.dirname(os.path.abspath(__file__))
 
 
@@ -28,18 +29,18 @@ package_dir = os.path.dirname(os.path.abspath(__file__))
 # The 'position' of an internal node is the minimum 'position' of its descendant leaves.
 class Tree(object):
     def __init__(self, arg, _position=0):
-        if not self.is_treelike(arg):
-            raise ValueError("A tree is constructed from a non-empty nested sequence of trees, strings, or bytestrings."
-                             " The presence of a string is mutually exclusive with the presence of a bytestring.")
+        #if not self.is_treelike(arg):
+        #    raise ValueError("A tree is constructed from a non-empty nested sequence of trees, strings, or bytestrings."
+        #                     " The presence of a string is mutually exclusive with the presence of a bytestring.")
         container_types = {tuple, list, Tree}
         self.type = None
         positions = []
-        delta = 0
+        delta = _position
         self.children = []
         for c in arg:
             positions.append(delta)
             if any(isinstance(c, tpe) for tpe in container_types):
-                infant = Tree(c, _position=_position + delta)
+                infant = Tree(c, _position=delta)
                 self.children.append(infant)
                 delta += infant.numleaves
             else:
@@ -53,6 +54,7 @@ class Tree(object):
     @classmethod
     def universal_type(cls, arg, leaf_predicate=lambda _: False):
         """ Returns the type common for all leaves in the tree-like (nested iterable) object, or None there is none. """
+
         def is_iterable(element):
             try:
                 iter(element)
@@ -97,15 +99,27 @@ class Tree(object):
         empty = self.type()
         return empty.join(c.flatten() if isinstance(c, Tree) else c for c in self.children)
 
+    def flatten_without_joining(self):
+        flat_list = []
+
+        def append_or_recurse(element):
+            if isinstance(element, Tree):
+                for c in element:
+                    append_or_recurse(c)
+            else:
+                flat_list.append(element)
+
+        append_or_recurse(self)
+        return Tree(flat_list)
+
     def list(self):
         return [c.list() if isinstance(c, Tree) else c for c in self.children]
 
-    def invert(self):  # Mutability
-        self.children.reverse()
-        self.positions = tuple(reversed(self.positions))
-        for c in self.children:
-            if isinstance(c, Tree):
-                c.invert()
+    def reversed(self):
+        return Tree(reversed(self))
+
+    def inverted(self):
+        return Tree([c.inverted() if isinstance(c, Tree) else c for c in self]).reversed()
 
     def offsets(self):
         t = tuple(c.numleaves if isinstance(c, Tree) else 1 for c in self)
@@ -116,10 +130,60 @@ class Tree(object):
         """ Applies the given unary function to every leaf in this tree. """
         return Tree([c.map(fn) if isinstance(c, Tree) else fn(c) for c in self])
 
-    def transliterate(self, dct):
-        """ Replaces every leaf by its associated value in the argument dictionary. Throw an exception if a key
-        does not exist. """
-        return Tree([c.transliterate(dct) if isinstance(c, Tree) else dct[c] for c in self])
+    def transliterate(self, codec: typing.Union[dict, bidict, 'Tree']) -> 'Tree':
+        """ Replaces every leaf by its associated value in the dictionary @codec. Throws an exception if a key does not
+        exist. If @codec is a pruning of a Tree of dictionaries T such that T shares the same structure as this Tree and
+        such that every node in @codec has either no children or the same amount of children as the corresponding node
+        in T, then every leaf below or at the node that corresponds to a dictionary will be replaced by the value that
+        the dictionary associates the current leaf value to. If more than one dictionary has the same descendant leaf,
+        the one closest to the descendant will take priority and the rest of the dictionaries will be ignored.
+        """
+        if isinstance(codec, dict) or isinstance(codec, bidict):
+            return Tree(c.transliterate(codec) if isinstance(c, Tree) else codec[c] for c in self)
+        lst = []
+        for child, childcodec in zip(self, codec):
+            if not isinstance(child, Tree):
+                lst.append(childcodec[child])
+            else:
+                lst.append(child.transliterate(childcodec))
+        return Tree(lst)
+
+    def replace(self, replacements: dict) -> 'Tree':
+        """ Returns the tree obtained by replacing leaves in this Tree by leaves in the dictionary @replacement. The
+        dictionary maps the index path to a leaf in this Tree to the content it should be replaced with. """
+        tree = self.list()
+        for path in self.leaf_indices():
+            current_children = tree
+            for i in path[:-1]:
+                current_children = current_children[i]
+            current_children[path[-1]] = replacements[path]
+        return Tree(tree)
+
+    @classmethod
+    def single_value_tree(cls, leafpaths, leafvalue=None):
+        """ Returns a Tree having the structure given by @leafpaths, which is a set of index paths to leaves. Each leaf
+        holds the (non-Tree) value @leafvalue. """
+        assert not isinstance(leafvalue, Tree)
+        children = []
+        sortedleafpaths = sorted(leafpaths, key=lambda x: x[0])
+        for path in sortedleafpaths:
+            if len(children) <= path[0]:
+                children += [leafvalue] * (path[0] - len(children) + 1)
+            if len(path) == 1:  # Base case: The destination child is a leaf
+                children[path[0]] = leafvalue
+            else:  # Inductive case: The destination child is a subtree
+                subtreeleafpaths = {p[1:] for p in leafpaths if p[0] == path[0]}
+                children[path[0]] = cls.single_value_tree(subtreeleafpaths, leafvalue)
+        return Tree(children)
+
+    def restructured(self, leafpaths: typing.Union[dict, bidict]) -> 'Tree':
+        """ Returns a Tree containing the same leaves as this Tree but has a structure defined by @leafpaths, which is
+        a dictionary mapping the index path to a leaf in this Tree to an index path to the same leaf in the new
+        Tree. """
+        restructured = self.single_value_tree(leafpaths.values())
+        leafpaths2content = {p2: self.reel_in(*p1) for p1, p2 in leafpaths.items()}
+        return restructured.replace(leafpaths2content)
+
 
     def leaf_indices(self):
         """ Returns a list of sequences of indices leading to a leaf, in a left-to-right depth-first-search manner. """
@@ -135,6 +199,12 @@ class Tree(object):
                 paths.append(tuple(stack + [i]))
         return paths
 
+    def get_leaf_path(self, idx):
+        return self.leaf_indices()[idx]
+
+    def get_leaf(self, idx):
+        return self.reel_in(*self.get_leaf_path(idx))
+
     def leaf_parent_indices(self):
         """ Returns a list of sequences of indices leading to a parent of a leaf, in a left-to-right
         depth-first-search manner. """
@@ -143,9 +213,19 @@ class Tree(object):
         parent_paths = [p[0] for p in groupby(parent_paths)]
         return parent_paths
 
+    def _position(self, *indexpath):
+        """ Returns the position of the leaf given by the path of indices. """
+        hook = self
+        for i in indexpath[:-1]:
+            hook = hook.children[i]
+        return hook.positions[-1]
+
     def graph(self):
         grph = dict()
-        for path in self.leaf_indices():
+        leaf_indices = self.leaf_indices()
+        for path in leaf_indices:
+            position = self._position(*path)
+            leaf_indices[position]
             originalpath = []
             subtree = self
             for nodeidx in path:
@@ -181,6 +261,21 @@ class Tree(object):
             return Tree([Tree.zip(c1, c2) for c1, c2 in zip(e1, e2)])
         else:
             return e1, e2
+
+    def traverse_preorder(self):
+        """ Returns a generator for every node in the tree by doing a depth-first pre-order traversal. """
+
+        def traverse(element):
+            if isinstance(element, Tree):
+                for child in element:
+                    g = traverse(child)
+                    for cc in g:
+                        yield next(cc)
+                    yield child
+            else:
+                yield element
+
+        return traverse(self)
 
     def contentwise_equals(self, other: 'Tree'):
         """ True iff the (byte-)strings resulting from flattening the trees are equal. """
@@ -231,10 +326,41 @@ class Tree(object):
         return treestr
 
     def __repr__(self):
-        childstrings = ["{}:{}".format(repr(c), p) for c, p in zip(self.children, self.positions)]
-        childrenstr = ",".join(childstrings)
-        treestr = "({})".format(childrenstr, self.positions)
-        return treestr
+        return self.__str__()
+
+
+class MutableTree(object):
+    """
+    This is what a tree probably should be: a container for a nested list which can be mutated.
+    After the user has defined a hierarchical ROM, he should create an instance of this class passing the ROM to the
+    constructor, and apply any sequence of operations on the instance by calling its appropriate methods.
+    """
+    # TODO should this inherit Tree instead? would probably get easier to define operations
+
+    def __init__(self, tree: Tree):
+        self._tree = tree
+        self.originalstate = dict()  # Calculate relation here. {(): (), (0,): (0,), (1,): (1,), ...}
+
+    def transliterate(self, leafmap=dict()):
+        """  """
+        for leafparentpath in self._tree.leaf_parent_indices():
+            pass
+
+    def join(self, path=(), delim=''):
+        """  """
+
+    def zip(self):
+        """  """
+
+    def reverse(self, path=()):
+        """ Reverse the children of the node that @path leads to. """
+        self._tree.children.reverse()
+
+    def invert(self):
+        """ Reverse the children of all nodes. """
+        for element in self._tree.traverse_preorder():
+            if isinstance(element, Tree):
+                element.reverse()
 
 
 def identity_dict(n):
@@ -249,7 +375,7 @@ def read_yaml(path):
     return dct
 
 
-def intersperse(bytestr, n):
+def intersperse(bytestr, n):  # Not to be confused with e.g. Haskell's intersperse
     return [bytestr[i::n] for i in range(n)]
 
 
@@ -260,13 +386,7 @@ def treemap(fn, element):  # TODO Memory complexity
     return fn(element)
 
 
-class Decoder(ABC):
-    @classmethod
-    def domain(cls, bytestr: bytes) -> Tree:
-        """ Returns any bytestring tree such that the flattening of it is equal to bytestr. By default returns a
-        bytestring tree containing a single leaf. """
-        return Tree([bytestr])
-
+class Codec(ABC):
     @classmethod
     def operate(cls, tree: Tree):  # FIXME? Could take a MutableTree inheriting Tree as a parameter, including positions
         """ Performs any number of operations on the bytestring tree (mutates the object). Note that the original tree
@@ -287,6 +407,11 @@ class Decoder(ABC):
         cls.operate(btree)
         stree = cls.decode(btree)
         return stree.flatten()
+
+    @classmethod
+    @abstractmethod
+    def encode(cls, s):
+        raise NotImplementedError("Encoder for {0} not implemented.".format(cls.__name__))
 
     @classmethod
     def correspondence(cls, bytestr: bytes) -> (Tree, Tree, dict):
@@ -313,13 +438,6 @@ class Decoder(ABC):
         return btree, stree, graph
 
 
-class Codec(Decoder):
-    @classmethod
-    @abstractmethod
-    def encode(cls, s):
-        raise NotImplementedError("Encoder for {0} not implemented.".format(cls.__name__))
-
-
 class Hexify(Codec):
     def encode(s):
         return bytes([int(s[i:i + 2], base=16) for i in range(0, len(s), 2)])
@@ -336,7 +454,7 @@ class HexifySpaces(Codec):
         return " ".join(Hexify.decode(bytes([n])) for n in bytestr)
 
 
-class ASCII(Decoder):
+class ASCII(Codec):
     def decode(bytestr):
         return "".join(chr(b) for b in bytestr)
 
@@ -347,12 +465,24 @@ class ASCII(Decoder):
         return btree.list(), stree.list(), indexmap
 
 
-class ReverseASCII(Decoder):
-    def decode(bytestr):
-        return "".join(reversed([chr(b) for b in bytestr]))
+class ReverseASCII(Codec):
+    @classmethod
+    def domain(cls, bytestr):
+        return Tree([bytes([b]) for b in bytestr])
+
+    @classmethod
+    def operate(cls, tree):
+        tree.invert()
+
+    @classmethod
+    def decode(cls, bytestuff):
+        if isinstance(bytestuff, bytes):
+            return "".join(reversed([chr(b) for b in bytestuff]))
+        else:
+            return Tree(bytestuff)
 
 
-class MonospaceASCIIByte(Decoder):
+class MonospaceASCIIByte(Codec):
     """ Like ASCII, but replaces any unprintable and non-monospace character
     with some other (non-ascii) monospace character. """
 
@@ -364,13 +494,13 @@ class MonospaceASCIIByte(Decoder):
         return chr(b)
 
 
-class MonospaceASCII(Decoder):  # TODO Codec
+class MonospaceASCII(Codec):  # TODO Codec
 
     def decode(bytestr):
         return "".join(MonospaceASCIIByte.decode(bytes([b])) for b in bytestr)
 
 
-class UppercaseASCII(Decoder):
+class UppercaseASCII(Codec):
     def decode(bytestr):
         return "".join(MonospaceASCIIByte.decode(bytes([b]).upper()) for b in bytestr)
 
@@ -381,7 +511,7 @@ class UppercaseASCII(Decoder):
         return btree.list(), stree.list(), indexmap
 
 
-class MajinTenseiII(Decoder):
+class MajinTenseiII(Codec):
     hexmap = os.path.join(package_dir, "resources/hexmap.yaml")
     transliter = read_yaml(hexmap)
 
@@ -389,16 +519,13 @@ class MajinTenseiII(Decoder):
         return "".join(MajinTenseiII.transliter[b] for b in bytestr)
 
 
-class Mt2GarbageTextPair(Decoder):
+class Mt2GarbageTextPair(Codec):
     def decode(bytestr):
         text, garbage = intersperse(bytestr, 2)
         result = MajinTenseiII.decode(text) + MonospaceASCII.decode(garbage)
         return result
 
 
-decoders = {c for (_, c) in
-            inspect.getmembers(sys.modules[__name__],
-                               lambda c: inspect.isclass(c) and issubclass(c, Decoder) and c not in {Decoder, Codec})}
-codecs = {c for (_, c) in
+ourcodecs = {c for (_, c) in
           inspect.getmembers(sys.modules[__name__],
                              lambda c: inspect.isclass(c) and issubclass(c, Codec) and c is not Codec)}

96c9cde8d99723464a6423260fb3a6b1572e4f76
Sebastian Olsson <sebelino7@gmail.com>
2017-08-01 22:23:21 +0200

    Updated P1 test_dump.

--- pyromhackit/roms/persona1usa/test_dump.py
+++ pyromhackit/roms/persona1usa/test_dump.py
@@ -1,31 +1,32 @@
 #!/usr/bin/env python
 
 import os
-from .dump import rom_path, Persona1Codec
 from pyromhackit.rom import ROM
-from pyromhackit.morphism import Morphism
+from pyromhackit.morphism import Hacker
 from pyromhackit.thousandcurses.codec import Tree
 import pytest
 
 package_dir = os.path.dirname(os.path.abspath(__file__))
 
+path_tensi = os.path.join(package_dir, "resources/copyrighted/TENSI.BIN")
 
-@pytest.mark.skipif(not os.path.isfile(rom_path), reason="Copyright infringement")
+@pytest.mark.skipif(not os.path.isfile(path_tensi), reason="Copyright infringement")
 def test_read_rom():
-    ROM(rom_path)
+    ROM(path_tensi)
 
 
-@pytest.mark.skipif(not os.path.isfile(rom_path), reason="Copyright infringement")
+@pytest.mark.skipif(not os.path.isfile(path_tensi), reason="Copyright infringement")
+@pytest.mark.skip()
 class TestWord(object):
     def setup_class(self):
-        rom = ROM(rom_path)
+        rom = ROM(path_tensi)
         idx = 25704*2  # Index of "How"
         wordlength = 6
         rom = rom[idx:idx+wordlength]
-        self.morphism = Morphism(rom, Persona1Codec)
+        self.irom = Hacker(rom)
 
     def test_src(self):
-        assert self.morphism.src == ROM(b'\x00\xe7\x01\x0f\x01\x17')
+        assert self.irom.src == ROM(b'\x00\xe7\x01\x0f\x01\x17')
 
     def test_dst(self):
         assert self.morphism.dst == 'How'
@@ -36,23 +37,23 @@ class TestWord(object):
     def test_dsttree(self):
         assert self.morphism.dsttree == Tree(['H', 'o', 'w'])
 
-    @pytest.mark.parametrize("srcindex, expected", [
+    @pytest.mark.parametrize("srcindex, expected_dstindices", [
         (0, {0}),
-        (1, {1}),
-        (2, {2}),
+        (1, {0}),
+        (2, {1}),
+        (3, {1}),
+        (4, {2}),
+        (5, {2}),
     ])
-    @pytest.mark.skip()
-    def test_source_diffusion(self, srcindex, expected):
-        assert self.morphism.source_diffusion(srcindex) == expected
+    def test_source_diffusion(self, srcindex, expected_dstindices):
+        assert self.morphism.source_diffusion(srcindex) == expected_dstindices
 
-    @pytest.mark.parametrize("bindex, sindex", [
-        (0, 0),
-        (1, 0),
-        (2, 1),
-        (3, 1),
-        (4, 2),
-        (5, 2),
+    @pytest.mark.parametrize("bindexpath, sindexpath", [
+        ((0,), (0,)),
+        ((1,), (1,)),
+        ((2,), (2,)),
     ])
-    @pytest.mark.skip()
-    def test_graph(self, bindex, sindex):
-        assert self.morphism.graph[bindex] == sindex
+    def test_graph(self, bindexpath, sindexpath):
+        assert self.morphism.graph[bindexpath] == sindexpath
+
+# Dump E0.txt, ..., E4.txt

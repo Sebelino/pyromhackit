2804948bdd686314311783d9a74f758d39c57ffa
Sebastian Olsson <sebelino7@gmail.com>
2017-09-08 19:56:50 +0200

    Increased test coverage for Selection.reveal.

--- pyromhackit/test/test_selection.py
+++ pyromhackit/test/test_selection.py
@@ -300,6 +300,10 @@ class TestThreeRevealedIntervals(object):
         self.v.reveal(2, 3)
         assert self.v.revealed == [slice(0, 3), slice(4, 6), slice(9, 10)]
 
+    def test_reveal_slice_fully(self):
+        self.v.reveal(2, 4)
+        assert self.v.revealed == [slice(0, 6), slice(9, 10)]
+
     @pytest.mark.skip()
     def test_reveal_partially(self):
         self.v.reveal_partially(1, 9, 1)

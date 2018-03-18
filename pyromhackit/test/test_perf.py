c97fed06cb72486498f0bd7ca3a81799e8ce8607
Sebastian Olsson <sebelino7@gmail.com>
2018-03-18 18:04:06 +0100

    Updated perf tests.

--- pyromhackit/test/test_perf.py
+++ pyromhackit/test/test_perf.py
@@ -15,18 +15,18 @@ class TestExclude(object):
 
     @staticmethod
     def exclude_n_times(selection, n):
-        # revealed = [(0, 1), (2, 3), ..., (n-2, n-1)]
+        # revealed = [(0, 1), (2, 3), ..., (2n-2, 2n-1), (2n, 100000)]
         for i in range(0, n):
             selection.exclude(2 * i + 1, 2 * i + 2)
 
-    def test_exclude_500_times(self, benchmark):
-        benchmark(self.exclude_n_times, self.v, 500)
+    def test_exclude_2pow0_times(self, benchmark):
+        benchmark(self.exclude_n_times, self.v, 500 * 2 ** 0)
 
-    def test_exclude_1000_times(self, benchmark):
-        benchmark(self.exclude_n_times, self.v, 1000)
+    def test_exclude_2pow1_times(self, benchmark):
+        benchmark(self.exclude_n_times, self.v, 500 * 2 ** 1)
 
-    def test_exclude_2000_times(self, benchmark):
-        benchmark(self.exclude_n_times, self.v, 2000)
+    def test_exclude_2pow2_times(self, benchmark):
+        benchmark(self.exclude_n_times, self.v, 500 * 2 ** 2)
 
 
 class TestInclude(object):
@@ -39,14 +39,85 @@ class TestInclude(object):
         for i in range(0, n):
             selection.include(2 * i, 2 * i + 1)
 
-    def test_include_500_times(self, benchmark):
-        benchmark(self.include_n_times, self.v, 500)
+    def test_include_2pow0_times(self, benchmark):
+        benchmark(self.include_n_times, self.v, 500 * 2 ** 0)
 
-    def test_include_1000_times(self, benchmark):
-        benchmark(self.include_n_times, self.v, 1000)
+    def test_include_2pow1_times(self, benchmark):
+        benchmark(self.include_n_times, self.v, 500 * 2 ** 1)
 
-    def test_include_2000_times(self, benchmark):
-        benchmark(self.include_n_times, self.v, 2000)
+    def test_include_2pow2_times(self, benchmark):
+        benchmark(self.include_n_times, self.v, 500 * 2 ** 2)
+
+
+class TestSubslice(object):
+    def setup(self):
+        self.without_gaps = Selection(universe=slice(0, 100000))
+        self.with_gaps = Selection(universe=slice(0, 100000), intervals=list(range(100000 + 1)))
+
+    @staticmethod
+    def subslice_n_times(selection, n):
+        for i in range(0, n):
+            selection.subslice(2 * i, 2 * i + 1)
+
+    def test_without_gaps(self, benchmark):
+        benchmark(self.subslice_n_times, self.without_gaps, 2 ** 2)
+
+    def test_with_gaps(self, benchmark):
+        benchmark(self.subslice_n_times, self.with_gaps, 2 ** 2)
+
+
+class TestIncludePartiallyFromLeft(object):
+    def setup(self):
+        self.v = Selection(universe=slice(0, 100000), revealed=[])
+
+    @staticmethod
+    def do_n_times(selection, n):
+        # Include partially from right (0, 1), (1, 2), (2, 3), ...
+        for i in range(0, n):
+            selection._include_partially_from_left(2 * i, 2 * i + 1, 1)
+
+    @pytest.mark.skip()
+    def test_2pow0_times(self, benchmark):
+        benchmark(self.do_n_times, self.v, 500 * 2 ** 0)
+
+    @pytest.mark.skip()
+    def test_2pow1_times(self, benchmark):
+        benchmark(self.do_n_times, self.v, 500 * 2 ** 1)
+
+    @pytest.mark.skip()
+    def test_2pow2_times(self, benchmark):
+        benchmark(self.do_n_times, self.v, 500 * 2 ** 2)
+
+    @pytest.mark.skip()
+    def test_2pow3_times(self, benchmark):
+        benchmark(self.do_n_times, self.v, 500 * 2 ** 3)
+
+
+class TestIncludePartiallyFromRight(object):
+    def setup(self):
+        self.v = Selection(universe=slice(0, 100000), revealed=[])
+
+    @staticmethod
+    def do_n_times(selection, n):
+        # Include partially from right (0, 1), (1, 2), (2, 3), ...
+        for i in range(0, n):
+            selection._include_partially_from_right(2 * i, 2 * i + 1, 1)
+
+    @pytest.mark.skip()
+    def test_2pow0_times(self, benchmark):
+        benchmark(self.do_n_times, self.v, 500 * 2 ** 0)
+
+    @pytest.mark.skip()
+    def test_2pow1_times(self, benchmark):
+        benchmark(self.do_n_times, self.v, 500 * 2 ** 1)
+
+    @pytest.mark.skip()
+    def test_2pow2_times(self, benchmark):
+        benchmark(self.do_n_times, self.v, 500 * 2 ** 2)
+
+    @pytest.mark.skip()
+    def test_2pow3_times(self, benchmark):
+        benchmark(self.do_n_times, self.v, 500 * 2 ** 3)
 
 
 class TestIncludePartially(object):
@@ -55,14 +126,16 @@ class TestIncludePartially(object):
 
     @staticmethod
     def do_n_times(selection, n):
-        # revealed = [(0, 1), (2, 3), ..., (n-2, n-1)]
         for i in range(0, n):
             selection.include_partially(2 * i, 2 * i + 1, 1)
 
-    def test_include_2000_times(self, benchmark):
+    def test_1000_times(self, benchmark):
+        benchmark(self.do_n_times, self.v, 1000)
+
+    def test_2000_times(self, benchmark):
         benchmark(self.do_n_times, self.v, 2000)
 
-    def test_include_4000_times(self, benchmark):
+    def test_4000_times(self, benchmark):
         benchmark(self.do_n_times, self.v, 4000)
 
 
@@ -102,6 +175,11 @@ class TestComplement(object):
                       revealed=[slice(2 * i, 2 * i + 1) for i in range(4000)])
         benchmark(v.complement)
 
+    def test_complement_8000(self, benchmark):
+        v = Selection(universe=slice(0, 2 * 8000),
+                      revealed=[slice(2 * i, 2 * i + 1) for i in range(8000)])
+        benchmark(v.complement)
+
 
 class TestPreviousSlice(object):
     def setup(self):
@@ -118,21 +196,27 @@ class TestPreviousSlice(object):
         for i in range(0, n):
             selection._previous_slice(slice(2 * i + 1, 2 * i + 2))
 
+    @pytest.mark.skip()
     def test_previous_slice_50_covered(self, benchmark):
         benchmark(self.covered_previous_slice_n_times, self.v, 50)
 
+    @pytest.mark.skip()
     def test_previous_slice_100_covered(self, benchmark):
         benchmark(self.covered_previous_slice_n_times, self.v, 100)
 
+    @pytest.mark.skip()
     def test_previous_slice_200_covered(self, benchmark):
         benchmark(self.covered_previous_slice_n_times, self.v, 200)
 
+    @pytest.mark.skip()
     def test_previous_slice_50_revealed(self, benchmark):
         benchmark(self.revealed_previous_slice_n_times, self.v, 50)
 
+    @pytest.mark.skip()
     def test_previous_slice_100_revealed(self, benchmark):
         benchmark(self.revealed_previous_slice_n_times, self.v, 100)
 
+    @pytest.mark.skip()
     def test_previous_slice_200_revealed(self, benchmark):
         benchmark(self.revealed_previous_slice_n_times, self.v, 200)
 
@@ -160,5 +244,28 @@ class TestEnglishDictionaryBasedIdentifier(object):
         benchmark(self.identifier.str2selection, corpus)
 
     def test_str2selection_2pow4(self, benchmark):
-        corpus = self.corpus * 2**4
+        corpus = self.corpus * 2 ** 4
+        benchmark(self.identifier.str2selection, corpus)
+
+
+class TestEnglishDictionaryBasedIdentifierTolerated(object):
+    def setup(self):
+        self.identifier = EnglishDictionaryBasedIdentifier(tolerated_char_count=5)
+        # self.corpus = lorem.ipsum()[:1000]
+        self.corpus = ("hello " * 1000)[:1000]
+
+    def test_str2selection_tolerated_2pow0(self, benchmark):
+        corpus = self.corpus * 2 ** 0
+        benchmark(self.identifier.str2selection, corpus)
+
+    def test_str2selection_tolerated_2pow1(self, benchmark):
+        corpus = self.corpus * 2 ** 1
+        benchmark(self.identifier.str2selection, corpus)
+
+    def test_str2selection_tolerated_2pow2(self, benchmark):
+        corpus = self.corpus * 2 ** 2
+        benchmark(self.identifier.str2selection, corpus)
+
+    def test_str2selection_tolerated_2pow3(self, benchmark):
+        corpus = self.corpus * 2 ** 3
         benchmark(self.identifier.str2selection, corpus)

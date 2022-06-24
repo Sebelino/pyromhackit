f0244e30e8a28120c71e4484bae09799b80695e9
Sebastian Olsson <sebelino7@gmail.com>
2022-06-24 14:20:45 +0200

    fix: Fix range error

--- v2/findscript/findscript.py
+++ v2/findscript/findscript.py
@@ -9,7 +9,7 @@ class Rom:
     def find(self, searchstring: str):
         bytestring = searchstring.encode()
         for offset in range(256):
-            rot_bytestring = bytes([b + offset for b in bytestring])
+            rot_bytestring = bytes([(b + offset) % 256 for b in bytestring])
             index = self.content.find(rot_bytestring)
             if index != -1:
                 break

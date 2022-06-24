ee5decc3c8e980a8b4ac785375e7661492d24597
Sebastian Olsson <sebelino7@gmail.com>
2022-06-24 14:43:47 +0200

    feat: Support showing offset in search result

--- v2/findscript/findscript.py
+++ v2/findscript/findscript.py
@@ -1,3 +1,22 @@
+class SearchResult:
+    def __init__(self, start: int, end: int, offset: int):
+        self._start = start
+        self._end = end
+        self._offset = offset
+
+    @property
+    def start(self):
+        return self._start
+
+    @property
+    def end(self):
+        return self._end
+
+    @property
+    def offset(self):
+        return self._offset
+
+
 class Rom:
     def __init__(self, content: bytes):
         self._content = content
@@ -9,13 +28,25 @@ class Rom:
     def find(self, searchstring: str):
         bytestring = searchstring.encode()
         for offset in range(256):
-            rot_bytestring = bytes([(b + offset) % 256 for b in bytestring])
+            rot_bytestring = self._rotate(bytestring, offset)
             index = self.content.find(rot_bytestring)
             if index != -1:
                 break
         else:
             raise NotImplementedError
-        return index, index + len(bytestring)
+        return SearchResult(index, index + len(bytestring), offset)
+
+    def rotate(self, offset: int):
+        rotated = self._rotate(self.content, offset)
+        return Rom(rotated)
+
+    @staticmethod
+    def _rotate(bytestring: bytes, offset: int):
+        return bytes([(b + offset) % 256 for b in bytestring])
+
+    def save(self, filename: str):
+        with open(filename, 'wb') as f:
+            f.write(self.content)
 
 
 def load(filename: str):
--- v2/findscript/test_findscript.py
+++ v2/findscript/test_findscript.py
@@ -1,13 +1,17 @@
-from findscript import Rom
+from findscript import Rom, load
 
 
 def test_findscript_plain():
     rom = Rom(b"hoyPLAYERdoy")
-    start, end = rom.find("PLAYER")
-    assert (start, end) == (3, 9)
+    search_result = rom.find("PLAYER")
+    assert search_result.start == 3
+    assert search_result.end == 9
+    assert search_result.offset == 0
 
 
 def test_findscript_rot4():
     rom = Rom(b"ls}TPE]IVhs}")
-    start, end = rom.find("PLAYER")
-    assert (start, end) == (3, 9)
+    search_result = rom.find("PLAYER")
+    assert search_result.start == 3
+    assert search_result.end == 9
+    assert search_result.offset == 4

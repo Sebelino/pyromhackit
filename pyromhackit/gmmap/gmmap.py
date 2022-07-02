df8af309fe79f399746054c2c8d0dbf745fb5501
Sebastian Olsson <sebelino7@gmail.com>
2022-07-02 14:13:34 +0200

    refactor(gmmap): Move class into separate file

--- pyromhackit/gmmap/gmmap.py
+++ pyromhackit/gmmap/gmmap.py
@@ -2,10 +2,6 @@ from abc import ABCMeta, abstractmethod
 
 import mmap
 
-from pyromhackit.gmmap.bytestring_sourced_string_mmap import BytestringSourcedStringMmap
-from pyromhackit.gmmap.selective_gmmap import SelectiveGMmap
-from pyromhackit.gslice.selection import Selection
-
 
 class GMmap(metaclass=ABCMeta):
     """ Generalized mmap. While a regular mmap stores a non-empty sequence of bytes, a GMmap stores a potentially empty
@@ -76,20 +72,3 @@ class GMmap(metaclass=ABCMeta):
     def __len__(self):  # Final
         """ :return The number of elements in the sequence. """
         return self._length
-
-
-class SelectiveBytestringSourcedStringMmap(SelectiveGMmap, BytestringSourcedStringMmap):
-
-    def __init__(self, bytestring_iterator, codec):
-        super(SelectiveBytestringSourcedStringMmap, self).__init__(bytestring_iterator, codec)
-        self._selection = Selection(universe=slice(0, self._length))
-
-    @property
-    def selection(self) -> Selection:
-        return self._selection
-
-    def _nonvirtualint2physical(self, location: int) -> slice:
-        return slice(4 * location, 4 * (location + 1))
-
-    def _nonvirtualselection2physical(self, location: Selection) -> Selection:
-        return location * 4

9331265212b8f9cb3da2df151910901f40141005
Sebastian Olsson <sebelino7@gmail.com>
2022-07-05 11:47:13 +0200

    feat(semantics): Soft-code implementation

--- pyromhackit/semantics/finder.py
+++ pyromhackit/semantics/finder.py
@@ -5,13 +5,6 @@ from pyromhackit.topology.simple_topology import SimpleTopology
 class SimpleMonobyteFinder:
     @staticmethod
     def find(bs: bytes) -> Semantics:
-        codec = {
-            b"H": "H",
-            b"e": "e",
-            b"l": "l",
-            b"o": "o",
-            b"&": "&",
-            b"$": "$",
-        }
+        codec = {bytes([b]): bytes([b]).decode() for b in bs}
         topology = SimpleTopology(1)
         return Semantics(topology=topology, codec=codec)
--- pyromhackit/semantics/test_semantics.py
+++ pyromhackit/semantics/test_semantics.py
@@ -2,7 +2,7 @@ from .finder import SimpleMonobyteFinder
 from .semantics import Semantics
 
 
-def test_finder():
+def test_finder_hello():
     finder = SimpleMonobyteFinder()
     bytestring = b"&&Hello$$$"
     returned = finder.find(bytestring)
@@ -18,3 +18,22 @@ def test_finder():
     assert b"".join(byteslist) == bytestring
     stringlist = [returned.codec[bs] for bs in byteslist]
     assert stringlist == ["&", "&", "H", "e", "l", "l", "o", "$", "$", "$"]
+
+
+def test_finder_world():
+    finder = SimpleMonobyteFinder()
+    bytestring = b"&&World"
+    returned = finder.find(bytestring)
+    assert isinstance(returned, Semantics)
+    assert returned.codec.keys() == {b"&", b"W", b"o", b"r", b"l", b"d"}
+    assert {
+        (b"W", "W"),
+        (b"o", "o"),
+        (b"r", "r"),
+        (b"l", "l"),
+        (b"d", "d"),
+    }.issubset(returned.codec.items())
+    byteslist = returned.topology.structure(bytestring)
+    assert b"".join(byteslist) == bytestring
+    stringlist = [returned.codec[bs] for bs in byteslist]
+    assert stringlist == ["&", "&", "W", "o", "r", "l", "d"]

7a7125038d99bbed5ac4f5337321853330cf7957
Sebastian Olsson <sebelino7@gmail.com>
2022-07-05 14:36:52 +0200

    fix(semantics): Correct resource path

--- pyromhackit/semantics/english_dictionary.py
+++ pyromhackit/semantics/english_dictionary.py
@@ -1,9 +1,12 @@
+import os
 from typing import Iterator
+
+package_dir = os.path.dirname(os.path.abspath(__file__))
 
 
 class EnglishDictionary:
     def __init__(self):
-        with open("pyromhackit/stringsearch/resources/cracklib-small-subset.txt") as f:
+        with open(os.path.join(package_dir, "../stringsearch/resources/cracklib-small-subset.txt")) as f:
             self._words = frozenset(word.strip() for word in f.read().strip().split())
 
     def iterbytestrings(self) -> Iterator[bytes]:

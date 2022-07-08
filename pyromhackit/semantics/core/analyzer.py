b43a68e4206105f9cd4e99c621b9f948b3236b5a
Sebastian Olsson <sebelino7@gmail.com>
2022-07-08 17:54:54 +0200

    refactor(semantics): Move caching logic inside module

--- pyromhackit/semantics/core/analyzer.py
+++ pyromhackit/semantics/core/analyzer.py
@@ -1,6 +1,27 @@
+import json
 from typing import Dict, Optional
 
-from pyromhackit.semantics.core.persist_to_file import persist_to_file
+
+def persist_to_file(path: str):
+    def decorator(original_func):
+        try:
+            with open(path, 'r') as f:
+                cache = json.load(f)
+        except (IOError, ValueError) as e:
+            cache = dict()
+
+        def new_func(cache_param: bytes, *args):
+            cache_param_str = repr(cache_param)
+            if cache_param_str not in cache:
+                dct = original_func(cache_param, *args)
+                cache[cache_param_str] = {repr(word): wordcount for word, wordcount in dct.items()}
+                with open(path, "w") as f:
+                    json.dump(cache, f)
+            return cache[cache_param_str]
+
+        return new_func
+
+    return decorator
 
 
 class Analyzer:

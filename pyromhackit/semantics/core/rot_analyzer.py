957856026fc9b125131ad2e7a828ba6d68160971
Sebastian Olsson <sebelino7@gmail.com>
2022-07-09 12:49:14 +0200

    perf(semantics): Implement caching in RotAnalyzer

--- pyromhackit/semantics/core/rot_analyzer.py
+++ pyromhackit/semantics/core/rot_analyzer.py
@@ -1,6 +1,29 @@
+import json
 from typing import Dict, Optional
 
 from .analyzer import Analyzer
+
+
+def persist_to_file(path: str):
+    def decorator(original_func):
+        try:
+            with open(path, 'r') as f:
+                cache = json.load(f)
+        except (IOError, ValueError) as e:
+            cache = dict()
+
+        def new_func(cache_param: bytes, *args):
+            cache_param_str = repr(cache_param)
+            if cache_param_str not in cache:
+                dct = original_func(cache_param, *args)
+                cache[cache_param_str] = {offset: {repr(w): c for w, c in d.items()} for offset, d in dct.items()}
+                with open(path, "w") as f:
+                    json.dump(cache, f)
+            return cache[cache_param_str]
+
+        return new_func
+
+    return decorator
 
 
 class RotAnalyzer:
@@ -16,15 +39,20 @@ class RotAnalyzer:
             d[bytes([(b - offset) % 256])] = chr(b)
         return d
 
-    def all_word_frequencies(self, bs: bytes) -> Dict[int, Dict[bytes, int]]:
+    @staticmethod
+    @persist_to_file(path="/tmp/all_word_frequencies.json")
+    def all_word_frequencies_static(bs: bytes, analyzer: Analyzer) -> Dict[int, Dict[bytes, int]]:
         freqs = dict()
         for offset in range(256):
             rotated_bs = bytes([(b + offset) % 256 for b in bs])
-            freq = self._analyzer.word_frequency(rotated_bs)
+            freq = analyzer.word_frequency(rotated_bs)
             if not freq:
                 continue
             freqs[offset] = freq
         return freqs
+
+    def all_word_frequencies(self, bs: bytes) -> Dict[int, Dict[bytes, int]]:
+        return self.all_word_frequencies_static(bs, self._analyzer)
 
     def find_rot(self, bs) -> Optional[Dict[bytes, str]]:
         freqs = self.all_word_frequencies(bs)

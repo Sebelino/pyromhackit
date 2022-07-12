c43e2e724eaab3fcc510561872a7b6fdafd465d6
Sebastian Olsson <sebelino7@gmail.com>
2022-07-12 11:29:34 +0200

    feat(smb): Add selection to SMB dumping script

--- pyromhackit/roms/mario/dump.py
+++ pyromhackit/roms/mario/dump.py
@@ -2,6 +2,7 @@
 
 from pyromhackit.rom import ROM
 from pyromhackit.semantics.rotating_monobyte.finder import RotatingMonobyteFinder
+from pyromhackit.stringsearch.identify import EnglishDictionaryBasedIdentifier
 
 if __name__ == '__main__':
     rom = ROM("copyrighted/mario.nes")
@@ -9,5 +10,7 @@ if __name__ == '__main__':
     finder = RotatingMonobyteFinder()
     semantics = finder.find(bs)
     irom_string = ''.join(semantics.codec[b] for b in semantics.topology.structure(bs))
-    print("Script mixed with data:")
-    print(irom_string)
+    identifier = EnglishDictionaryBasedIdentifier(tolerated_char_count=0)
+    selection = identifier.str2selection(irom_string)
+    print("Script:")
+    print(selection.select(irom_string))
